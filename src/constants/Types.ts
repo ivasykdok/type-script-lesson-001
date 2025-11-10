5c9c351e43cb620be1a8860e42453e5513e3670f
Roman Ivasyk <ivasykdok@@email.com>
2025-11-10 11:50:56 +0200

    lesson-6 update

--- src/constants/Types.ts
+++ src/constants/Types.ts
@@ -1,9 +1,8 @@
-import { priorities, statuses, TaskSchema } from "./constants";
+import { TaskSchema, statuses, priorities } from "./constants";
 import { z } from "zod";
 
 export type Status = (typeof statuses)[number];
 export type Priority = (typeof priorities)[number];
-
 export type Task = z.infer<typeof TaskSchema>;
 
 export type FilterOptions = {
--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,14 +1,13 @@
 import { z } from "zod";
-import { Priority, Status } from "./Types";
 
 export const statuses = ["todo", "in_progress", "done"] as const;
 export const priorities = ["low", "medium", "high"] as const;
 
 export const StatusSchema = z.enum(statuses);
-export const StatusPriority = z.enum(priorities);
+export const PrioritySchema = z.enum(priorities);
 
-export const DEFAULT_STATUS: Status = "todo";
-export const DEFAULT_PRIORITY: Priority = "low";
+export const DEFAULT_STATUS = "todo";
+export const DEFAULT_PRIORITY = "low";
 
 export const TaskSchema = z.object({
   id: z.string(),
@@ -17,5 +16,5 @@ export const TaskSchema = z.object({
   createdAt: z.union([z.string(), z.date()]),
   deadline: z.union([z.string(), z.date()]),
   status: StatusSchema.default(DEFAULT_STATUS),
-  priority: StatusPriority.default(DEFAULT_PRIORITY),
+  priority: PrioritySchema.default(DEFAULT_PRIORITY),
 });
