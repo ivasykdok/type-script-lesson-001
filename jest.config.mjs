cf24176569e9c14d80494a81e2b41fde346e17c3
Roman Ivasyk <ivasykdok@gmail.com>
2025-11-25 00:54:46 +0200

    lesson-13 insert tests

---
+++ jest.config.mjs
@@ -0,0 +1,10 @@
+const config = {
+  preset: "ts-jest/presets/default-esm",
+  testEnvironment: "node",
+  extensionsToTreatAsEsm: [".ts"],
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+  testMatch: ["**/*.test.ts"],
+};
+export default config;
